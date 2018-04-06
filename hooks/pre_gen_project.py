a8887b44e446515e8ad2cdf52d4ba0de1ea6689b
Neutrollized <glen.yu@gmail.com>
2018-04-06 10:29:43 -0400

    test

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,7 +1,13 @@
 import os
 import sys
 from random import *
+from cookiecutter.main import cookiecutter
 
 # set env variable 'winrm_host_port' to a random in between 55986 and 59986
-p = randint(55986, 59986)
-os.environ["winrm_host_port"] = str(p)
+#p = randint(55986, 59986)
+#os.environ["winrm_host_port"] = str(p)
+
+cookiecutter(
+    'cookiecutter-django',
+    extra_context={'rand_winrm_port': randint(55986, 59986)}
+)
