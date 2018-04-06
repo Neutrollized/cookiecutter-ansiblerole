19add841c8c69253d57d8996d59197d82e24ebbb
Neutrollized <glen.yu@gmail.com>
2018-04-06 10:17:50 -0400

    fixed..?

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,4 +3,5 @@ import sys
 import random
 
 # set env variable 'winrm_host_port' to a random in between 55986 and 59986
-os.environ["winrm_host_port"] = randint(55986,59986) 
+p = randint(55986, 59986)
+os.environ["winrm_host_port"] = p
